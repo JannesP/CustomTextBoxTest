cf2a8827929fb4d8c33776beb574efd23cac8f3a
JannesP <jannespeters.privat@gmail.com>
2016-06-30 14:35:55 +0200

    Initial Commit.

---
+++ CustomTextBoxTest/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace CustomTextBoxTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.regExAutocompleteTextBox1 = new CustomTextBoxTest.RegExAutocompleteTextBox();
+            this.SuspendLayout();
+            // 
+            // regExAutocompleteTextBox1
+            // 
+            this.regExAutocompleteTextBox1.Location = new System.Drawing.Point(13, 13);
+            this.regExAutocompleteTextBox1.Name = "regExAutocompleteTextBox1";
+            this.regExAutocompleteTextBox1.Size = new System.Drawing.Size(423, 20);
+            this.regExAutocompleteTextBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 45);
+            this.Controls.Add(this.regExAutocompleteTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private RegExAutocompleteTextBox regExAutocompleteTextBox1;
+    }
+}
+
---
+++ CustomTextBoxTest/RegExAutocompleteTextBox.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomTextBoxTest
+{
+    class RegExAutocompleteTextBox : TextBox
+    {
+        private Dictionary<string, string> searchIndex = new Dictionary<string, string>();
+        private char[] searchSplitters = { '-', ' ', '#', ';', '.', ':', ',', '_', '\'', '+' };
+
+        private bool isAutoCompletionTextChanged = false;
+
+        public RegExAutocompleteTextBox()
+        {
+            searchIndex.Add("hallo", "hai");
+            searchIndex.Add("dasdas", "hai");
+            searchIndex.Add("haldasdlo", "hai");
+            searchIndex.Add("haldasdalo", "hai");
+            searchIndex.Add("halsdalo", "hai");
+            searchIndex.Add("hadasallo", "hai");
+            searchIndex.Add("hdallo", "hai");
+            searchIndex.Add("hsdaallo", "hai");
+            searchIndex.Add("hasallo", "hai");
+            searchIndex.Add("hasdallo", "hai");
+            searchIndex.Add("haallo", "hai");
+            searchIndex.Add("hsallo", "hai");
+            searchIndex.Add("hdasdallo", "hai");
+            searchIndex.Add("haadallo", "hai");
+            searchIndex.Add("hadsdallo", "hai");
+            searchIndex.Add("hdaaallo", "hai");
+            searchIndex.Add("hdadasdasdllo", "hai");
+            searchIndex.Add("dadsads", "hai");
+        }
+
+        protected override void OnTextChanged(EventArgs e)
+        {
+            base.OnTextChanged(e);
+            long ticksBefore = DateTime.Now.Ticks;
+            var result = GetAutoCompleteList(base.Text);
+            Console.WriteLine("Time: " + ((DateTime.Now.Ticks - ticksBefore) / TimeSpan.TicksPerMillisecond));
+            foreach (KeyValuePair<string, string> kvp in result)
+            {
+                Console.WriteLine(kvp.Key);
+            }
+        }
+
+        protected Dictionary<string, string> GetAutoCompleteList(string searchText)
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+
+            foreach (KeyValuePair<string, string> kvp in searchIndex)
+            {
+                string[] keySearches = kvp.Key.Split(searchSplitters);
+                string[] searchTextParts = searchText.Split(searchSplitters);
+                bool[] textPartExists = new bool[searchTextParts.Length];
+                for (int i = 0; i < textPartExists.Length; i++)
+                {
+                    textPartExists[i] = false;
+                }
+                for (int i = 0; i < searchTextParts.Length; i++)
+                {
+                    string currSearchText = searchTextParts[i];
+                    for (int x = 0; x < keySearches.Length; x++)
+                    {
+                        string currKeySearch = keySearches[x];
+
+                        if (MatchesRegEx(currSearchText, currKeySearch))
+                        {
+                            textPartExists[i] = true;
+                            break;
+                        }
+
+                    }
+                }
+                bool isMatch = true;
+                for (int i = 0; i < textPartExists.Length; i++)
+                {
+                    if (!textPartExists[i])
+                    {
+                        isMatch = false;
+                        break;
+                    }
+                }
+                if (isMatch) result.Add(kvp.Key, kvp.Value);
+            }
+
+            return result;
+        }
+
+        protected bool MatchesRegEx(string stringToSearch, string stringToBeSearched)
+        {
+            long ticksBefore = DateTime.Now.Ticks;
+            bool result = false;
+            string regExPattern = "(" + stringToSearch + ")";
+            result = System.Text.RegularExpressions.Regex.IsMatch(stringToBeSearched, regExPattern, System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            Console.WriteLine("Time-single: " + ((DateTime.Now.Ticks - ticksBefore) / TimeSpan.TicksPerMillisecond));
+            return result;
+        }
+
+    }
+}
